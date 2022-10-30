9ab97f306575028aeba0cf0a5dbb85101340c9db
Justin McLawhorn <jwmclawhorn@gmail.com>
2022-10-30 12:52:05 -0400

    Initial commit: base layout, tiers, & login page

---
+++ webapp/src/components/navbar/NavbarGetStarted.svelte
@@ -0,0 +1,19 @@
+<a href="/tiers">
+  Get Started
+</a>
+
+<style>
+  a {
+    color: inherit;
+    font-weight: bolder;
+    background-color: var(--ltpurple);
+    padding: 0.25rem 0.75rem;
+    border-radius: 500px;
+  }
+
+  a:hover {
+    box-shadow: 0px 2px 10px #222;
+    background-color: var(--pink);
+  }
+  
+</style>
---
+++ webapp/src/components/navbar/NavbarLogin.svelte
@@ -0,0 +1,20 @@
+<a href="/login">
+  Login
+</a>
+
+<style>
+  a {
+    color: #999;
+    font-weight: bolder;
+    
+  }
+
+  a:visited {
+
+  }
+
+  a:hover {
+    color: #BBB;
+  }
+
+</style>
